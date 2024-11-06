a69c83a67d1065b83d59e6d302ece76d7aa18c67
zlimaz <miguell1040z@gmail.com>
2024-11-06 20:54:53 -0300

    segundo exercicio do livro

---
+++ EDA1-EXERCICIOS/Livro2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+/*o programa calculará o produto de três numeros inteiros*/
+    int x;
+    int y;
+    int z;
+    int resultado;
+
+    printf("digite um numero inteiro\n");
+    scanf("%d", &x);
+
+    printf("digite o segundo numero inteiro\n");
+    scanf("%d", &y);
+
+    printf("digite o terceiro numero inteiro\n");
+    scanf("%d", &z);
+
+    printf("o resultado e: %d\n", x*y*z);
+    
+    
+}
